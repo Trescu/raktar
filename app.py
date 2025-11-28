c2d028819a69e43122e7881c0f6efdb28a36f3d6
Tálas Barnabás <barnabastalas@gmail.com>
2025-11-28 09:37:04 +0100

    6. Feladat

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 
 app = Flask(__name__)
@@ -31,6 +31,41 @@ def butor_list():
      rows = conn.execute("SELECT * FROM butor ORDER BY cikkszam").fetchall()
      conn.close()
      return render_template("butor_list.html", butorok=rows)
+
+
+@app.route("/butor/uj", methods=["GET", "POST"])
+def butor_create():
+    if request.method == "POST":
+        cikkszam = request.form.get("cikkszam", "").strip()
+        nev = request.form.get("nev", "").strip()
+        suly = request.form.get("suly", "").strip()
+        x = request.form.get("x", "").strip()
+        y = request.form.get("y", "").strip()
+        z = request.form.get("z", "").strip()
+
+
+        if not cikkszam:
+            flash("A cikkszám nem lehet üres!", "Hiba")
+            return redirect(url_for("butor_create"))
+
+
+        conn = get_db_connection()
+        existing = conn.execute("SELECT * FROM butor WHERE cikkszam = ?", (cikkszam,)).fetchone()
+        if existing:
+            conn.close()
+        flash("Ez a cikkszám már létezik!", "Hiba")
+        return redirect(url_for("butor_create"))
+
+
+
+        conn.execute("INSERT INTO butor (cikkszam, nev, suly, x, y, z) VALUES (?, ?, ?, ?, ?, ?)",(cikkszam, nev, suly, x, y, z))
+        conn.commit()
+        conn.close()
+
+
+        flash("Új bútor sikeresen hozzáadva!", "Sikeres")
+        return redirect(url_for("butor_list"))
+    return render_template("butor_form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
